d8c9b85bb7a6d8baf9e3b3e1b015201d394245ff
Sreeram2711 <sreeramsep27@gmail.com>
2025-06-28 22:29:01 +0530

    learning.java

---
+++ Week 2_PLSQL_Exercises,  JUnit_Basic Testing Exercises/Junit, Mockito and SLJ4/JUnit_Basic Testing Exercises/Junit_basic testing exercise/Ex1 Setting Up JUnit/settingup.java
@@ -0,0 +1,14 @@
+package learning.junit.learning.maven;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class learningjunit {
+
+
+@Test
+public void testAdd() {
+    assertEquals(5,5);
+
+}
+}
