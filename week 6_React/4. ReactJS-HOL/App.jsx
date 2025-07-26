e8069d0daa4de249325ae9447867b02b48aa3b39
SREERAM D <sreeramsep27@gmail.com>
2025-07-26 21:04:20 +0530

    Create App.jsx

---
+++ week 6_React/4. ReactJS-HOL/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Posts from "./components/Posts";
+import "./index.css"; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Blog App</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
