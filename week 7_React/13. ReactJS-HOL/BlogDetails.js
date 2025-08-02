e3bcd0b82fb68fc7f22a6c460fc346e997bf47aa
SREERAM D <sreeramsep27@gmail.com>
2025-08-02 22:51:39 +0530

    Create BlogDetails.js

---
+++ week 7_React/13. ReactJS-HOL/BlogDetails.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function BlogDetails() {
+  return (
+    <div>
+      <h2>Blog Details</h2>
+
+      <h3>React Learning</h3>
+      <p><strong>Stephen Biz</strong></p>
+      <p>Welcome to learning React!</p>
+
+      <h3>Installation</h3>
+      <p><strong>Schwezdneier</strong></p>
+      <p>You can install React from npm.</p>
+    </div>
+  );
+}
+
+export default BlogDetails;
